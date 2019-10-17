ca27f574d2f027060dc3a206b57f0a15f80dc3be
oo0ooo0 <52806458+oo0ooo0@users.noreply.github.com>
2019-10-17 18:02:57 +0900

    1017

---
+++ _source/javascript/008_Modifying the DOM with JS/index.js
@@ -0,0 +1,6 @@
+// 008_Events and event handlers 
+// 바닐라 자바스크립트 #2-3 Modifying the DOM with JS 노마드 코더 Nomad Coders
+
+const title = document.querySelector("#title");
+
+title.innerHTML = "Hi! From JS";
