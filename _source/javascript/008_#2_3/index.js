7e13364864b73d72a6d62029eb34c5fdacc1c933
oo0ooo0 <52806458+oo0ooo0@users.noreply.github.com>
2019-10-18 16:49:36 +0900

    1018

--- _source/javascript/008_#2_3/index.js
+++ _source/javascript/008_#2_3/index.js
@@ -2,5 +2,9 @@
 // 바닐라 자바스크립트 #2-3 Modifying the DOM with JS 노마드 코더 Nomad Coders
 
 const title = document.querySelector("#title");
+    
+function handleClick() {
+title.style.color = "red";
+}
 
-title.innerHTML = "Hi! From JS";
+title.addEventListener("click", handleClick);
