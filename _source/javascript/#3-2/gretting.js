1e450a224e4d45edb858db6f1c7ec3eb8e81a21b
oo0ooo0 <52806458+oo0ooo0@users.noreply.github.com>
2019-11-04 16:54:18 +0900

    1104

--- _source/javascript/#3-2/gretting.js
+++ _source/javascript/#3-2/gretting.js
@@ -1,6 +1,13 @@
 const from = document.querySelector(".js-form"),
   input = form.querySelector("input");
 
+function loadName() {
+  const crrentUser = localStorage.getItem(USER_LS);
+
+  if (crrentUser === null) {
+  } else {
+  }
+}
 function init() {}
 
 init();
