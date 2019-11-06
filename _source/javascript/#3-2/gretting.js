b6be5e8a22b41b1c578f45f1e7c288ff903778aa
oo0ooo0 <52806458+oo0ooo0@users.noreply.github.com>
2019-11-06 09:11:09 +0900

    #3-3

--- _source/javascript/#3-2/gretting.js
+++ _source/javascript/#3-2/gretting.js
@@ -5,6 +5,8 @@ const from = document.querySelector(".js-form"),
 const USER_LS = "crrentUser",
   SHOWING_ON = "showing";
 
+function askForName() {}
+
 function paintGreeting(text) {
   form.classlist.remove(SHOWING_ON);
   greeting.classList.add(SHOWING_ON);
@@ -15,10 +17,14 @@ function loadName() {
   const crrentUser = localStorage.getItem(USER_LS);
 
   if (crrentUser === null) {
+    askForName();
   } else {
     paintGreeting(crrentUser);
   }
 }
-function init() {}
+
+function init() {
+  loadName();
+}
 
 init();
