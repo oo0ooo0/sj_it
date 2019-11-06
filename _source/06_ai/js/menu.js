bc0c1cb4db10a3521fa9b70a03fed35bc8a87e2c
oo0ooo0 <52806458+oo0ooo0@users.noreply.github.com>
2019-11-06 11:34:12 +0900

    -

--- _source/06_ai/js/menu.js
+++ _source/06_ai/js/menu.js
@@ -67,15 +67,28 @@ window.onload = function() {
       //jq
 // console.log("greenBarTob" +$("#greenBar").offset().top);
 // console.log("greenBarL" +$("#greenBar").offset().left);
-console.log("greenBarTob::" +$("#greenBar").position().top);
-console.log("greenBarLeft::" +$("#greenBar").position().left);
-console.log ("height");
+// console.log("greenBarTop::" +$("#greenBar").position().top);
+// console.log("greenBarLeft::" +$("#greenBar").position().left);
+// console.log ("height");
 
 
 //clear
 $("#gnb1 a").css("color","#009070");
 $("#greenBar").css("width",$("#gnb1").width());
-
+console.log("#gnb1 width:" + $("#gnb1").width());
+console.log("#gnb2 width:" + $("#gnb2").width());
+var menu1X = $("#gnb1").position().left;
+var menu2X = $("#gnb2").position().left;
+var menu3X = $("#gnb3").position().left;
+var menu4X = $("#gnb4").position().left;
+var menu5X = $("#gnb5").position().left;
+var menu6X = $("#gnb6").position().left;
+var menu7X = $("#gnb7").position().left;
+var menu8X = $("#gnb8").position().left;
+console.log("#gnb1 위치:" + $("#gnb1").position().left);
+console.log("#gnb2 위치:" + $("#gnb2").position().left);
+console.log("#gnb3 위치:" + $("#gnb3").position().left);
+console.log("#gnb4 위치:" + $("#gnb4").position().left);
 // $("header #desktopMenu li:first-child a").css("color","#00970");
 // console.log("#gnb li:first-child").width;
 // console.log("#gnb li:last-child").width;
@@ -94,17 +107,69 @@ $("#greenBar").css("width",$("#gnb1").width());
 //   console.log("click");
 // });
 
+$("#gnb1").on("click", function(){
+  $("#greenBar").css("width",$("#gnb1").width());
+  $("#greenBar").css("left",menu1X);
+  // $("#gnb1 li a").css("color", "#000");
+
+  $("#gnb1 a").css("color", "#009070");
+});
+
 $("#gnb2").on("click", function(){
   $("#greenBar").css("width",$("#gnb2").width());
-  $("#gnb1 a").css("color", "#000");
+  // $("#greenBar").css("left",menu2X);
+  $("#greenBar").animate({left: menu2X});
+  $("#gnb1 a").css("color", "black");
   $("#gnb2 a").css("color", "#009070");
+});
+
+$("#gnb3").on("click", function(){
+  $("#greenBar").css("width",$("#gnb3").width());
+  $("#greenBar").css("left",menu3X);
+  $("#gnb2 a").css("color", "black");
+  $("#gnb3 a").css("color", "#009070");
+});
+
+$("#gnb4").on("click", function(){
+  $("#greenBar").css("width",$("#gnb4").width());
+  $("#greenBar").css("left",menu4X);
+  $("#gnb2 a").css("color", "black");
+  $("#gnb4 a").css("color", "#009070");
+});
+
+$("#gnb5").on("click", function(){
+  $("#greenBar").css("width",$("#gnb5").width());
+  $("#greenBar").css("left",menu5X);
+  $("#gnb4 a").css("color", "black");
+  $("#gnb5 a").css("color", "#009070");
+});
+
+$("#gnb6").on("click", function(){
+  $("#greenBar").css("width",$("#gnb6").width());
+  $("#greenBar").css("left",menu6X);
+  $("#gnb5 a").css("color", "black");
+  $("#gnb6 a").css("color", "#009070");
+});
+  
+$("#gnb7").on("click", function(){
+  $("#greenBar").css("width",$("#gnb7").width());
+  $("#greenBar").css("left",menu7X);
+  $("#gnb6 a").css("color", "black");
+  $("#gnb7 a").css("color", "#009070");
+});
+  
+$("#gnb8").on("click", function(){
+  $("#greenBar").css("width",$("#gnb8").width());
+  $("#greenBar").css("left",menu8X);
+  $("#gnb7 a").css("color", "black");
+  $("#gnb8 a").css("color", "#009070");
 });
   
 
 
 //practice
 // $("html").animate({scrollTop:800}, "slow");
-console.log("sec2  "+$("#section_schedule").offset().top);
+// console.log("sec2  "+$("#section_schedule").offset().top);
 };
 
 
