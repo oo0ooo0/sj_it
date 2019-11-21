888f821a4b842dcd24c42887e85ab841d9f858c5
oo0ooo0 <52806458+oo0ooo0@users.noreply.github.com>
2019-11-21 11:11:30 +0900

    메뉴 스크롤 에니메이션

--- _teture_source/2_aihackathon2018/js/menu.js
+++ _teture_source/2_aihackathon2018/js/menu.js
@@ -1,3 +1,5 @@
+
+window.onload = function(){
 // $(document).ready(function(){
 
 // });
@@ -14,27 +16,62 @@ $("#triger").click(function(){
 
 
 
-   //w3scool Curtain Menu
-    $("#mobile_gnb").css("width","170px");
+//    //w3scool Curtain Menu
+//     $("#mobile_gnb").css("width","170px");
 
 
-    // //    //w3scool _Off-Canvas Menu
+    // //    //w3scool _Off-Canvas Menu_1
     // $("#mobile_gnb").css("width","170px");
-    // $("body").css("margin-right","170px");
+    // $("body").css("marginright","170px");
+   
+   
+   
+    // //    //w3scool _Off-Canvas Menu_2
+    $("#mobile_gnb").css("width","170px");
+    $("body").animate({"right":"170px"},500); //
 
 });
+
+
+
 
 $("#m_gnb_close").click(function(){
     // $("#m_gnb_close").hide();
     // $("#mobile_gnb").hide();
    
     //w3scool Curtain Menu
-    $("#mobile_gnb").css("width","0px");
+    // $("#mobile_gnb").css("width","0px");
 
 
-//    //w3scool _Off-Canvas Menu
+//    //w3scool _Off-Canvas Menu1
 //     $("#mobile_gnb").css("width","0px");
 //     $("body").css("margin-right","0px");
 
+//    //w3scool _Off-Canvas Menu2
+    $("#mobile_gnb").css("width","0px");
+    $("body").animate({"right":"0px"},500);
+
 }); 
 
+
+
+$("#gnb li").click(function(){
+    
+    // console.log(this);
+    // console.log(this.id);
+
+})
+
+
+for (var i = 1; i<=7;){
+$("#gnbMenu"+i).click(function(){
+    console.log(this.id);
+    // $("html").animate({scrollTop:$("#section1").offset().top},"slow");
+})
+}
+
+
+
+
+
+}
