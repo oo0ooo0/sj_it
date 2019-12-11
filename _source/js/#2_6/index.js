f35006ac48618046388bc472dd067165b05a875e
oo0ooo0 <nyanyanyangnyang@gmail.com>
2019-12-11 09:41:26 +0900

    -

--- _source/js/#2_6/index.js
+++ _source/js/#2_6/index.js
@@ -1,14 +1,25 @@
-const title = document.querySelector("#title");
 
+
+
+const title = document.querySelector("h1");
 const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "rgb(70, 73, 94)";
+const OTHER_COLOR = "rgb(200, 73, 94)";
+
+const superEventHandler = {};
+
+
+//click
+
 
 function handleClick() {
   const currentColor = title.style.color;
   if (currentColor === BASE_COLOR) {
     title.style.color = OTHER_COLOR;
+    title.innerHTML = "Hi click!"
   } else {
     title.style.color = BASE_COLOR;
+    title.innerHTML = "Hi click!!!"
+
   }
   console.log(title.style.color);
 }
@@ -21,14 +32,39 @@ function init() {
 
 init();
 
-// wifi handeler
-function handleOffline() {
-  console.log("lalalalal");
+
+
+
+
+
+// handleResize
+function handleResize() {
+  title.innerHTML = "Hi handleResize!"
 }
+
+window.addEventListener("resize", handleResize);
+
+
+
+//mouse over out
+title.addEventListener("mouseover", function( event ) {   
+  // highlight the mouseover target
+  event.target.style.color = "orange";
+  title.innerHTML = "Hi mouse!"
+
+});
+
+
+title.addEventListener("mouseout", function( event ) {   
+  // highlight the mouseover target
+  event.target.style.color = "olive";
+  title.innerHTML = "bye mouse!"
+
+});
+
+
 
 function handleOnline() {
   console.log("welcome back");
 }
-
-window.addEventListener("offline", handleOffline);
 window.addEventListener("online", handleOnline);
